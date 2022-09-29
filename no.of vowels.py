9705d8a3251d426d2668f7aadbd632dfa2b2fd24
Lahari-00 <112242529+Lahari-00@users.noreply.github.com>
2022-09-29 14:21:28 +0530

    Add files via upload

---
+++ no.of vowels.py
@@ -0,0 +1,7 @@
+s=input("enter the string")
+vowels=0
+for i in s:
+    if(i=='a' or i=='A' or i=='e' or i=='E' or i=='o' or i=='O' or i=='u' or i=='U'):
+        vowels=vowels+1
+print("no.of vowels are:",vowels)
+    
