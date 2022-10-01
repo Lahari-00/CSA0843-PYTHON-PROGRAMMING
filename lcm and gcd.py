75ebfe2e971a8a986711457ddb7ab6953a7e6e14
Lahari-00 <112242529+Lahari-00@users.noreply.github.com>
2022-10-01 08:50:41 +0530

    Add files via upload

---
+++ lcm and gcd.py
@@ -0,0 +1,22 @@
+num=int(input("Enter the number of numbers "))
+if(num==2):
+    a=int(input("Enter First Number: "))
+    b=int(input("Enter Second Number: "))
+    for i in range(1,a+1):
+        if(a%i==0 and b%i==0):
+            gcd=i
+    print("GCD of %d and %d is %d"%(a,b,gcd))
+    lcm=(a*b)/gcd
+    print("LCM of %d and %d is %d"%(a,b,lcm))
+elif(num==3):
+    a=int(input("Enter First Number: "))
+    b=int(input("Enter Second Number: "))
+    c=int(input("Enter Third Number: "))
+    for i in range(1,a+1):
+        if(a%i==0 and b%i==0 and c%i==0):
+            gcd=i
+            print("GCD of %d, %d and %d is %d"%(a,b,c,gcd))
+            lcm=(a*b*c)/gcd
+            print("LCM of %d, %d and %d is %d"%(a,b,c,lcm))
+else:
+    print("INVALID INPUT")
